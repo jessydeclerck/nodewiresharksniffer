bd1e121ea5d210bef4447ab07d3e85f1d5f6c3b5
Jessy <declerck.jessy@gmail.com>
2019-10-09 22:54:11 +0200

    Try to decode + example error logs

--- main.js
+++ main.js
@@ -19,7 +19,7 @@ const tsharkParams = [
   "-T",
   "json",
   "-ni",
-  "4",
+  "any",
   "-e",
   "ip.src",
   "-e",
@@ -51,9 +51,11 @@ const tsharkParams = [
   //   "-e",
   //   "tcp.segment",
   "-o",
-  "tcp.desegment_tcp_streams:false"
+  "tcp.desegment_tcp_streams:false",
   // "host",
   // "54.154.81.32",
+  "port",
+  "5555"
 ];
 let tsharkProcess;
 
@@ -67,11 +69,27 @@ oboe(tsharkProcess.stdout).node("layers", data => {
   // if (!_.isEmpty(data)) {
   let ipsrc = data["ip.src"];
   let ipdst = data["ip.dst"];
+  let srcport = data["tcp.srcport"]
   let payload = data["tcp.payload"];
   //TODO use port to determine is message is from client or from server
   console.log(`src ${ipsrc}`);
   console.log(`dst ${ipdst}`);
-  console.log(`data ${payload}`);
+  console.log(`src port ${srcport}`);
+  if(payload){
+    let context = "fromclient";
+    if(srcport == 5555){
+      context = "fromserver"
+    }
+    axios.post("http://localhost:5000/decoder/".concat(context), payload[0].replace(/:/g, '')).then(resp => {
+      console.log(`data ${payload[0].replace(/:/g, '')}`);
+      console.log(resp.data);
+    }, err => {
+      // console.log(err);
+      console.error(`error ${payload[0].replace(/:/g, '')}`);
+    });
+  }
+
+
   console.log("\n");
   // gameData = JSON.parse(data);
   // console.log(data.ip);
@@ -83,3 +101,5 @@ tsharkProcess.stderr.pipe(process.stdout);
 tsharkProcess.on("close", code => {
   console.log(`child process exited with code ${code}`);
 });
+
+
