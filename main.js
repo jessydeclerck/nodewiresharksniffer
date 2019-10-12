9ce4954f82c81cb816861bab51a9b9847197f2de
Jessy Declerck <declerck.jessy@gmail.com>
2019-10-12 13:47:06 +0200

    split messages test

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const { spawn } = require("child_process");
 const oboe = require("oboe");
 const _ = require("lodash");
 const axios = require("axios");
-const readMsgId = require("./payloadReader").readMsgId;
+const payloadReader = require("./payloadReader");
 const msgIds = require("./neededMsg").msgIds;
 const treasureHelper = require("./treasureHelper");
 /**
@@ -22,7 +22,7 @@ const tsharkParams = [
   "-T",
   "json",
   "-ni",
-  "any",
+  "2",
   "-e",
   "tcp.srcport",
   "-e",
@@ -48,9 +48,15 @@ tsharkProcess.on("error", err => {
 oboe(tsharkProcess.stdout).node("layers", async data => {
   let srcport = data["tcp.srcport"];
   let payload = data["tcp.payload"];
+  const MSGID_DATALEN_SIZE = 2;
+  const dataLenLen = payloadReader.getDataLenLen(payloadReader.getHeaderFromPayload(payload));
+  const HEADER_SIZE = MSGID_DATALEN_SIZE + dataLenLen;
+  let dataLen = payloadReader.readDataLen(payload);
+  console.log(`dataLength: ${dataLen}`);
+  console.log(`payload length without header length: ${Buffer.byteLength(payload) - HEADER_SIZE}`);
   if (payload) {
     let dataPayload = payload[0].replace(/:/g, "");
-    let msgId = readMsgId(dataPayload);
+    let msgId = payloadReader.readMsgId(dataPayload);
     if (!msgIds.includes(msgId)) return;
     let context = getContext(srcport);
     let decodedMessage = await decodePayload(dataPayload, context);
@@ -59,11 +65,16 @@ oboe(tsharkProcess.stdout).node("layers", async data => {
   }
 });
 
+
 tsharkProcess.stderr.pipe(process.stdout);
 
 tsharkProcess.on("close", code => {
   console.log(`child process exited with code ${code}`);
 });
+
+function handleSplitMsg(){
+  //TODO decode if total length without header is equal to some of messages length
+}
 
 async function decodePayload(payload, context) {
   let response;
--- payloadReader.js
+++ payloadReader.js
@@ -1,7 +1,25 @@
+const HEADER_OFFSET = 2;
 module.exports = {
   readMsgId: function(payload) {
     let buffer = Buffer.from(payload, "hex");
-    let header = buffer.readInt16BE(0);
-    return header >> 2;
+    return getHeader(buffer) >> 2;
+  },
+  readDataLen: function(payload) {
+    let buffer = Buffer.from(payload, "hex");
+    let header = getHeader(buffer);
+    let dataLenLen = this.getDataLenLen(header);
+    console.log(`payload length: ${buffer.byteLength}`)
+    return buffer.readUIntBE(HEADER_OFFSET, dataLenLen);
+  },
+  getHeaderFromPayload: function(payload){
+    let buffer = Buffer.from(payload, "hex");
+    return getHeader(buffer);
+  },
+  getDataLenLen: function(header){
+    return header & 3;
   }
 };
+
+function getHeader(buffer){
+  return buffer.readInt16BE(0);
+}
