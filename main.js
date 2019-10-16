cb80160491b90e680b588222e1dde5879afc2840
Jessy Declerck <declerck.jessy@gmail.com>
2019-10-16 15:38:51 +0200

    pkg added and interface number have to be chosen

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const { spawn } = require("child_process");
+const { spawn, spawnSync } = require("child_process");
 const axios = require("axios");
 const payloadReader = require("./payloadReader");
 const msgIds = require("./neededMsg").msgIds;
@@ -6,6 +6,7 @@ const treasureHelper = require("./treasureHelper");
 const splittedMsgBuilder = require("./splittedMsgBuilder");
 const JSONStream = require("JSONStream");
 const indicesLoader = require("./indicesLoader");
+
 /**
  * on Linux, Unix, *BSD you can use
 
@@ -20,14 +21,12 @@ You can get the interface number with
 dumpcap -D -M
  */
 const tsharkParams = [
-  "-l",// flush stdout after each packet
-  "-p",// disable promiscuous mode
+  "-l", // flush stdout after each packet
+  "-p", // disable promiscuous mode
   "-Y",
   "tcp.len > 0",
   "-T",
   "json",
-  "-ni",
-  "2", //TODO interface to choose it or determine it programmaticaly
   "-e",
   "tcp.srcport",
   "-e",
@@ -35,53 +34,66 @@ const tsharkParams = [
   "-o", //https://www.wireshark.org/docs/dfref/t/tcp.html see not captured flag tcp.analysis.lost_segment
   "tcp.desegment_tcp_streams:true",
   "port",
-  "5555",
+  "5555"
 ];
 
-let stream = JSONStream.parse('._source.*');
-indicesLoader.loadIndices();
+let startHelper = interfaceNumber => {
+  let stream = JSONStream.parse("._source.*");
+  indicesLoader.loadIndices();
+  let tsharkProcess = spawn("tshark", ["-ni", interfaceNumber, ...tsharkParams]);
 
-let tsharkProcess = spawn("tshark", tsharkParams);
+  tsharkProcess.stdout.pipe(stream);
+  tsharkProcess.stderr.pipe(process.stderr);
 
-tsharkProcess.on("error", err => {
-  console.error("error while starting tshark", err);
-});
+  tsharkProcess.on("error", err => {
+    console.error("error while starting tshark", err);
+  });
+  tsharkProcess.on("close", code => {
+    console.log(`child process exited with code ${code}`);
+  });
 
-const MSGID_DATALEN_SIZE = 2;
-
-
-tsharkProcess.stdout.pipe(stream);
-
-stream.on('data', async data => {
-  let srcport = data["tcp.srcport"];
-  let payload = data["tcp.payload"];
-  if (payload) {
-    let dataPayload = payload[0].replace(/:/g, "");;
-    if (splittedMsgBuilder.isSplittedMsgWaiting()) {
-      dataPayload = splittedMsgBuilder.tryAppendMsg(dataPayload);
-      if (Buffer.byteLength(dataPayload, "hex") >= splittedMsgBuilder.getTotalLength()) {
-        splittedMsgBuilder.resetSplittedMsg();
+  stream.on("data", async data => {
+    let srcport = data["tcp.srcport"];
+    let payload = data["tcp.payload"];
+    if (payload) {
+      let dataPayload = payload[0].replace(/:/g, "");
+      if (splittedMsgBuilder.isSplittedMsgWaiting()) {
+        dataPayload = splittedMsgBuilder.tryAppendMsg(dataPayload);
+        if (
+          Buffer.byteLength(dataPayload, "hex") >=
+          splittedMsgBuilder.getTotalLength()
+        ) {
+          splittedMsgBuilder.resetSplittedMsg();
+        }
+        if (splittedMsgBuilder.isSplittedMsgWaiting()) return;
+      } else {
+        handleSplitMsg(dataPayload);
+        if (splittedMsgBuilder.isSplittedMsgWaiting()) return;
       }
-      if (splittedMsgBuilder.isSplittedMsgWaiting()) return;
-    } else {
-      handleSplitMsg(dataPayload);
-      if (splittedMsgBuilder.isSplittedMsgWaiting()) return;
+      let msgId = payloadReader.readMsgId(dataPayload);
+      if (!msgIds.includes(msgId)) return;
+      let context = getContext(srcport);
+      let decodedMessage = await decodePayload(dataPayload, context);
+      if (msgId != 226) console.log(decodedMessage);
+      treasureHelper.handleData(decodedMessage);
     }
-    let msgId = payloadReader.readMsgId(dataPayload);
-    if (!msgIds.includes(msgId)) return;
-    let context = getContext(srcport);
-    let decodedMessage = await decodePayload(dataPayload, context);
-    if(msgId != 226) console.log(decodedMessage);
-    treasureHelper.handleData(decodedMessage);
+  });
+
+};
+
+spawnSync("tshark", ["-D"], {stdio: "inherit"});
+
+let stdin = process.openStdin();
+console.log("Enter interface number to listen :");
+stdin.on("data", d => {
+  if (isNaN(d)) {
+    console.log("Please enter a number");
+    return;
   }
+  startHelper(d.toString().replace(/(\r\n|\n|\r)/gm, ""));
 });
 
-tsharkProcess.stderr.pipe(process.stdout);
-
-tsharkProcess.on("close", code => {
-  console.log(`child process exited with code ${code}`);
-});
-
+const MSGID_DATALEN_SIZE = 2
 function handleSplitMsg(dataPayload) {
   const dataLenLen = payloadReader.getDataLenLen(
     payloadReader.getHeaderFromPayload(dataPayload)
