f23113cc8016da7eb7d3601937428d73fa8253d5
Jessy Declerck <declerck.jessy@gmail.com>
2019-10-17 16:00:53 +0200

    now works with custom sniffer

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const { spawn, spawnSync } = require("child_process");
+const { spawn } = require("child_process");
 const axios = require("axios");
 const payloadReader = require("./payloadReader");
 const msgIds = require("./neededMsg").msgIds;
@@ -6,96 +6,69 @@ const treasureHelper = require("./treasureHelper");
 const splittedMsgBuilder = require("./splittedMsgBuilder");
 const JSONStream = require("JSONStream");
 const indicesLoader = require("./indicesLoader");
+const readline = require("readline");
 
+let stream = JSONStream.parse();
 
-/**
- * on Linux, Unix, *BSD you can use
-
-tshark -ni any
-
-on Windows, any does not work, so you'll have to specify the interface ID or number
-
-tshark -ni 1 -ni 2 -ni 3 (this will work on Linux, Unix, *BSD as well)
-
-You can get the interface number with
-
-dumpcap -D -M
- */
-const tsharkParams = [
-  "-l", // flush stdout after each packet
-  "-p", // disable promiscuous mode
-  "-Y",
-  "tcp.len > 0",
-  "-T",
-  "json",
-  "-e",
-  "tcp.srcport",
-  "-e",
-  "tcp.payload",
-  // "-o", //https://www.wireshark.org/docs/dfref/t/tcp.html see not captured flag tcp.analysis.lost_segment
-  // "tcp.desegment_tcp_streams:true",
-  "port",
-  "5555"
-];
-
-let startHelper = interfaceNumber => {
-  let stream = JSONStream.parse("._source.*");
+let startHelper = () => {
   indicesLoader.loadIndices();
-  let tsharkProcess = spawn("tshark", ["-ni", interfaceNumber, ...tsharkParams]);
+  let snifferProcess = spawn("./sniffer/sniffer.exe");
 
-  tsharkProcess.stdout.pipe(stream);
-  tsharkProcess.stderr.pipe(process.stderr);
+  snifferProcess.stdout.pipe(process.stdout);
 
-  tsharkProcess.on("error", err => {
-    console.error("error while starting tshark", err);
-  });
-  tsharkProcess.on("close", code => {
-    console.log(`child process exited with code ${code}`);
+  let rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
   });
 
-  stream.on("data", async data => {
-    let srcport = data["tcp.srcport"];
-    let payload = data["tcp.payload"];
-    if (payload) {
-      let dataPayload = payload[0].replace(/:/g, "");
-      if (splittedMsgBuilder.isSplittedMsgWaiting()) { //TODO might block app
-        dataPayload = splittedMsgBuilder.tryAppendMsg(dataPayload);
-        if (
-          Buffer.byteLength(dataPayload, "hex") >=
-          splittedMsgBuilder.getTotalLength()
-        ) {
-          splittedMsgBuilder.resetSplittedMsg();
-        }
-        if (splittedMsgBuilder.isSplittedMsgWaiting()) return;
-      } else {
-        handleSplitMsg(dataPayload);
-        if (splittedMsgBuilder.isSplittedMsgWaiting()) return;
-      }
-      let msgId = payloadReader.readMsgId(dataPayload);
-      if (!msgIds.includes(msgId)) return;
-      let context = getContext(srcport);
-      let decodedMessage = await decodePayload(dataPayload, context);
-      console.log(decodedMessage);
-      treasureHelper.handleData(decodedMessage);
+  rl.on("line", function(line) {
+    if (isNaN(parseInt(line))) {
+      console.log("Please enter a number : ");
+      return;
     }
+    snifferProcess.stdin.write(parseInt(line) + "\n");
+    rl.removeAllListeners();
+    snifferProcess.stdout.on("data", data => {
+      if (data.includes("Listening")) {
+        console.log("Redirecting sniffer stdout...");
+        snifferProcess.stdout.unpipe(process.stdout);
+        snifferProcess.stdout.pipe(stream);
+        snifferProcess.removeAllListeners();
+      }
+    });
   });
-
 };
 
-spawnSync("tshark", ["-D"], {stdio: "inherit"});
+startHelper();
 
-let stdin = process.openStdin();
-console.log("Enter interface number to listen :");
-stdin.on("data", d => {
-  if (isNaN(d)) {
-    console.log("Please enter a number");
-    return;
+stream.on("data", async data => {
+  let srcport = data["srcport"];
+  let dataPayload = data["payload"];
+  if (dataPayload) {
+    // let dataPayload = payload[0].replace(/:/g, "");
+    if (splittedMsgBuilder.isSplittedMsgWaiting()) {
+      dataPayload = splittedMsgBuilder.tryAppendMsg(dataPayload);
+      if (
+        Buffer.byteLength(dataPayload, "hex") >=
+        splittedMsgBuilder.getTotalLength()
+      ) {
+        splittedMsgBuilder.resetSplittedMsg();
+      }
+      if (splittedMsgBuilder.isSplittedMsgWaiting()) return;
+    } else {
+      handleSplitMsg(dataPayload);
+      if (splittedMsgBuilder.isSplittedMsgWaiting()) return;
+    }
+    let msgId = payloadReader.readMsgId(dataPayload);
+    if (!msgIds.includes(msgId)) return;
+    let context = getContext(srcport);
+    let decodedMessage = await decodePayload(dataPayload, context);
+    if (msgId != 226) console.log(decodedMessage);
+    treasureHelper.handleData(decodedMessage);
   }
-  startHelper(d.toString().replace(/(\r\n|\n|\r)/gm, ""));
-  stdin.removeAllListeners();
 });
 
-const MSGID_DATALEN_SIZE = 2
+const MSGID_DATALEN_SIZE = 2;
 function handleSplitMsg(dataPayload) {
   const dataLenLen = payloadReader.getDataLenLen(
     payloadReader.getHeaderFromPayload(dataPayload)
